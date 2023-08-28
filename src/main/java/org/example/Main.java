9057a8d8a6ac42848862a3cc3c71d8d4a3bd1ed9
Hendrik Scholtz <schwarzbaer@gmx.biz>
2023-08-28 11:09:42 +0200

    feat: implemented all tests added corresponding methods in Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,10 +7,27 @@ public class Main {
 
     static class PasswordCheck {
         static final String[] badPasswordList = { "passwort", "password", "pw", "none" };
+        static final String allowedSpecialChars = ".,:;(){}[]+-*/!$%&=?<>";
         static final int minLength = 8;
 
         public static boolean checkLength(String password) {
-            return true;
+            return false;
+        }
+
+        public static boolean checkNumbers(String password) {
+            return false;
+        }
+
+        public static boolean checkCase(String password) {
+            return false;
+        }
+
+        public static boolean checkBadPassword(String password) {
+            return false;
+        }
+
+        public static boolean checkSpecialChars(String password) {
+            return false;
         }
     }
 }
--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -15,23 +15,111 @@ class MainTest {
         Assertions.assertFalse(actual);
     }
     @Test
-    void returnTrue_whenCheckLength_getsALongEnoughPassword() {}
+    void returnTrue_whenCheckLength_getsALongEnoughPassword() {
+        // given
+        String password = "#".repeat(Main.PasswordCheck.minLength);
+        // when
+        boolean actual = Main.PasswordCheck.checkLength(password);
+        // then
+        Assertions.assertTrue(actual);
+    }
     @Test
-    void returnFalse_whenCheckNumbers_getsAPasswordWithoutNumbers() {}
+    void returnFalse_whenCheckNumbers_getsAPasswordWithoutNumbers() {
+        // given
+        String password = "abcde";
+        // when
+        boolean actual = Main.PasswordCheck.checkNumbers(password);
+        // then
+        Assertions.assertFalse(actual);
+    }
     @Test
-    void returnTrue_whenCheckNumbers_getsAPasswordWithNumbers() {}
+    void returnTrue_whenCheckNumbers_getsAPasswordWithNumbers() {
+        // given
+        String password = "abcde123";
+        // when
+        boolean actual = Main.PasswordCheck.checkNumbers(password);
+        // then
+        Assertions.assertTrue(actual);
+    }
     @Test
-    void returnFalse_whenCheckCase_getsAPasswordWithLowerCaseOnly() {}
+    void returnFalse_whenCheckCase_getsAPasswordWithLowerCaseOnly() {
+        // given
+        String password = "abcde";
+        // when
+        boolean actual = Main.PasswordCheck.checkCase(password);
+        // then
+        Assertions.assertFalse(actual);
+    }
     @Test
-    void returnFalse_whenCheckCase_getsAPasswordWithUpperCaseOnly() {}
+    void returnFalse_whenCheckCase_getsAPasswordWithUpperCaseOnly() {
+        // given
+        String password = "ABCDE";
+        // when
+        boolean actual = Main.PasswordCheck.checkCase(password);
+        // then
+        Assertions.assertFalse(actual);
+    }
     @Test
-    void returnTrue_whenCheckCase_getsAPasswordWithUpperAndLowerCase() {}
+    void returnTrue_whenCheckCase_getsAPasswordWithUpperAndLowerCase() {
+        // given
+        String password = "AbcdE";
+        // when
+        boolean actual = Main.PasswordCheck.checkCase(password);
+        // then
+        Assertions.assertTrue(actual);
+    }
     @Test
-    void returnFalse_whenCheckBadPW_getsABadPassword1() {}
+    void returnFalse_whenCheckBadPW_getsABadPassword1() {
+        // given
+        String password = Main.PasswordCheck.badPasswordList[0];
+        // when
+        boolean actual = Main.PasswordCheck.checkBadPassword(password);
+        // then
+        Assertions.assertFalse(actual);
+    }
     @Test
-    void returnFalse_whenCheckBadPW_getsABadPassword2() {}
+    void returnFalse_whenCheckBadPW_getsABadPassword2() {
+        // given
+        String password = "12345";
+        // when
+        boolean actual = Main.PasswordCheck.checkBadPassword(password);
+        // then
+        Assertions.assertFalse(actual);
+    }
     @Test
-    void returnFalse_whenCheckBadPW_getsABadPassword3() {}
+    void returnTrue_whenCheckBadPW_getsANotBadPassword() {
+        // given
+        String password = "AGoodPassword";
+        // when
+        boolean actual = Main.PasswordCheck.checkBadPassword(password);
+        // then
+        Assertions.assertTrue(actual);
+    }
     @Test
-    void returnTrue_whenCheckBadPW_getsANotBadPassword() {}
+    void returnFalse_whenCheckSpecialChars_getsAPasswordWithoutSpecialChars() {
+        // given
+        String password = "abcde";
+        // when
+        boolean actual = Main.PasswordCheck.checkSpecialChars(password);
+        // then
+        Assertions.assertFalse(actual);
+    }
+    @Test
+    void returnFalse_whenCheckSpecialChars_getsAPasswordWithWrongSpecialChars() {
+        // given
+        String password = "abcde#";
+        // when
+        boolean actual = Main.PasswordCheck.checkSpecialChars(password);
+        // then
+        Assertions.assertFalse(actual);
+    }
+    @Test
+    void returnTrue_whenCheckSpecialChars_getsAPasswordWithAllowedSpecialChars() {
+        // given
+        String password = "abcde(){}";
+        // when
+        boolean actual = Main.PasswordCheck.checkSpecialChars(password);
+        // then
+        Assertions.assertTrue(actual);
+    }
 }
