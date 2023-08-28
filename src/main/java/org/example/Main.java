87336efec3f3c0d10973fe19b3f89de8c2e768b7
Hendrik Scholtz <schwarzbaer@gmx.biz>
2023-08-28 10:33:22 +0200

    feat: added some tests & a first methode

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,4 +4,13 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    static class PasswordCheck {
+        static final String[] badPasswordList = { "passwort", "password", "pw", "none" };
+        static final int minLength = 8;
+
+        public static boolean checkLength(String password) {
+            return true;
+        }
+    }
 }
--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -1,4 +1,37 @@
-import static org.junit.jupiter.api.Assertions.*;
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 class MainTest {
-  
+
+    @Test
+    void returnFalse_whenCheckLength_getsATooShortPassword() {
+        // given
+        String password = "#".repeat(Main.PasswordCheck.minLength-1);
+        // when
+        boolean actual = Main.PasswordCheck.checkLength(password);
+        // then
+        Assertions.assertFalse(actual);
+    }
+    @Test
+    void returnTrue_whenCheckLength_getsALongEnoughPassword() {}
+    @Test
+    void returnFalse_whenCheckNumbers_getsAPasswordWithoutNumbers() {}
+    @Test
+    void returnTrue_whenCheckNumbers_getsAPasswordWithNumbers() {}
+    @Test
+    void returnFalse_whenCheckCase_getsAPasswordWithLowerCaseOnly() {}
+    @Test
+    void returnFalse_whenCheckCase_getsAPasswordWithUpperCaseOnly() {}
+    @Test
+    void returnTrue_whenCheckCase_getsAPasswordWithUpperAndLowerCase() {}
+    @Test
+    void returnFalse_whenCheckBadPW_getsABadPassword1() {}
+    @Test
+    void returnFalse_whenCheckBadPW_getsABadPassword2() {}
+    @Test
+    void returnFalse_whenCheckBadPW_getsABadPassword3() {}
+    @Test
+    void returnTrue_whenCheckBadPW_getsANotBadPassword() {}
 }
